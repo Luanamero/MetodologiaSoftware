33a0de30f6a6d34b6fdbfd47314b424d3847f037
Luiz Henrique <luizhengs@gmail.com>
2025-08-04 11:26:24 -0300

    #bugfix: Saving in binary files instead of .json

--- med-app-java/src/main/java/com/medapp/infra/FileRepository.java
+++ med-app-java/src/main/java/com/medapp/infra/FileRepository.java
@@ -1,6 +1,5 @@
 package com.medapp.infra;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.medapp.models.User;
 import com.medapp.utils.storage.*;
 
@@ -9,7 +8,6 @@ import java.util.*;
 
 public class FileRepository implements Repository {
     private static final String USER_FOLDER = "users";
-    private final ObjectMapper mapper = new ObjectMapper();
 
     public FileRepository() {
         File folder = new File(USER_FOLDER);
@@ -20,22 +18,25 @@ public class FileRepository implements Repository {
 
     @Override
     public void saveUser(User user) {
-        File file = new File(USER_FOLDER, user.getUsername() + ".json");
-        
+        File file = new File(USER_FOLDER, user.getUsername() + ".bin");
+
         if (file.exists()) {
             throw new UserAlreadyExistsException(user.getUsername());
         }
-        
-        try {
-            mapper.writeValue(file, user);
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(user);
             System.out.println("Saved user to file: " + file.getName());
+
+        } catch (FileNotFoundException e) {
+            throw new StoragePermissionException("write file", file.getAbsolutePath());
+
+        } catch (EOFException e) {
+            throw new StorageCorruptedException(file.getName(), e);
+
         } catch (IOException e) {
             if (e.getMessage() != null && e.getMessage().toLowerCase().contains("no space left")) {
                 throw new InsufficientStorageSpaceException("save user");
-            }
-            if (e instanceof FileNotFoundException || 
-                (e.getMessage() != null && e.getMessage().toLowerCase().contains("permission denied"))) {
-                throw new StoragePermissionException("write file", file.getAbsolutePath());
             }
             throw new FileStorageException("write", file.getAbsolutePath(), e);
         }
@@ -43,19 +44,26 @@ public class FileRepository implements Repository {
 
     @Override
     public User loadUser(String username) {
-        File file = new File(USER_FOLDER, username + ".json");
+        File file = new File(USER_FOLDER, username + ".bin");
+
         if (!file.exists()) {
             throw new UserNotFoundException(username);
         }
-        
-        try {
-            return mapper.readValue(file, User.class);
-        } catch (IOException e) {
-            if (e instanceof FileNotFoundException || 
-                (e.getMessage() != null && e.getMessage().toLowerCase().contains("permission denied"))) {
-                throw new StoragePermissionException("read file", file.getAbsolutePath());
-            }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (User) ois.readObject();
+
+        } catch (FileNotFoundException e) {
+            throw new StoragePermissionException("read file", file.getAbsolutePath());
+
+        } catch (EOFException e) {
             throw new StorageCorruptedException(file.getName(), e);
+
+        } catch (ClassNotFoundException e) {
+            throw new StorageCorruptedException("Class not found during deserialization of user: " + username, e);
+
+        } catch (IOException e) {
+            throw new FileStorageException("read", file.getAbsolutePath(), e);
         }
     }
 
@@ -63,63 +71,70 @@ public class FileRepository implements Repository {
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         File folder = new File(USER_FOLDER);
-        
+
         if (!folder.exists()) {
             throw new StoragePermissionException("access directory", folder.getAbsolutePath());
         }
-        
-        File[] files = folder.listFiles((dir, name) -> name.endsWith(".json"));
 
-        if (files != null) {
-            for (File file : files) {
-                try {
-                    users.add(mapper.readValue(file, User.class));
-                } catch (IOException e) {
-                    System.out.println("Warning: Skipping corrupted file: " + file.getName() + " -> " + e.getMessage());
-                }
-            }
-        } else {
+        File[] files = folder.listFiles((dir, name) -> name.endsWith(".bin"));
+
+        if (files == null) {
             throw new StoragePermissionException("list files", folder.getAbsolutePath());
         }
+
+        for (File file : files) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                users.add((User) ois.readObject());
+            } catch (EOFException e) {
+                System.out.println("Warning: Skipping corrupted file (EOF): " + file.getName() + " -> " + e.getMessage());
+            } catch (ClassNotFoundException e) {
+                System.out.println("Warning: Class not found while reading: " + file.getName() + " -> " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Warning: I/O error while reading file: " + file.getName() + " -> " + e.getMessage());
+            }
+        }
+
 
         return users;
     }
 
     @Override
     public void deleteUser(String username) {
-        File file = new File(USER_FOLDER, username + ".json");
-        
+        File file = new File(USER_FOLDER, username + ".bin");
+
         if (!file.exists()) {
             throw new UserNotFoundException(username);
         }
-        
+
         if (!file.delete()) {
             throw new StoragePermissionException("delete file", file.getAbsolutePath());
         }
-        
+
         System.out.println("Deleted user file for: " + username);
     }
-    
+
     public void updateUser(User user) {
-        File file = new File(USER_FOLDER, user.getUsername() + ".json");
-        
+        File file = new File(USER_FOLDER, user.getUsername() + ".bin");
+
         if (!file.exists()) {
             throw new UserNotFoundException(user.getUsername());
         }
-        
-        try {
-            mapper.writeValue(file, user);
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(user);
             System.out.println("Updated user file: " + file.getName());
+
+        } catch (FileNotFoundException e) {
+            throw new StoragePermissionException("write file", file.getAbsolutePath());
+
+        } catch (EOFException e) {
+            throw new StorageCorruptedException(file.getName(), e);
+
         } catch (IOException e) {
             if (e.getMessage() != null && e.getMessage().toLowerCase().contains("no space left")) {
                 throw new InsufficientStorageSpaceException("update user");
-            }
-            if (e instanceof FileNotFoundException || 
-                (e.getMessage() != null && e.getMessage().toLowerCase().contains("permission denied"))) {
-                throw new StoragePermissionException("write file", file.getAbsolutePath());
             }
             throw new FileStorageException("update", file.getAbsolutePath(), e);
         }
     }
 }
-
